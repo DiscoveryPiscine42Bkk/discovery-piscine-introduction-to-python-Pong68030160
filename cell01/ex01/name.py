211bcc734caa77e1dbc2d446c2dd307f338c86dc
Pong68030160 <pratchayakornjati009@gmail.com>
2025-06-16 10:53:03 +0000

    age.py

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+last_name = "Jati"
+fast_namt = "Pratchayakorn"
+whole_name = (last_name + " " + fast_namt)
+print(whole_name)
---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+first_name = input ("กุรณากรอกชื่อของคุณ: ")
+Last_name = input ("กรุณากรอกนาสกุลของคุณ: ")
+print("ชื่อและนามสกุลของคุณคือ: " + first_name + "  " + Last_name)
